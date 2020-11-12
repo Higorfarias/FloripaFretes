2aa4ac08bc6af22481755a04906538bdd32057ee
Higor <higornf2009@gmail.com>
2020-11-12 20:47:14 -0300

    Banco de test H2

--- floripafretes/backend/src/main/java/br/sc/senai/floripafretes/entities/Frete.java
+++ floripafretes/backend/src/main/java/br/sc/senai/floripafretes/entities/Frete.java
@@ -1,7 +1,15 @@
 package br.sc.senai.floripafretes.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Frete {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String descricao;

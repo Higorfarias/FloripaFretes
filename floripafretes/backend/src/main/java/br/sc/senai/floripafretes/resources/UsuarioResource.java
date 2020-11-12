0de1f6d9089b7e2d5269c22b1e96c9d05c107c7b
Higor <higornf2009@gmail.com>
2020-11-12 20:57:59 -0300

    Listando todos Usuarios

--- floripafretes/backend/src/main/java/br/sc/senai/floripafretes/resources/UsuarioResource.java
+++ floripafretes/backend/src/main/java/br/sc/senai/floripafretes/resources/UsuarioResource.java
@@ -1,8 +1,11 @@
 package br.sc.senai.floripafretes.resources;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +15,14 @@ import br.sc.senai.floripafretes.entities.Usuario;
 import br.sc.senai.floripafretes.repositories.UsuarioRepository;
 
 @RestController
-@RequestMapping(value = "/usuarios")
+@RequestMapping("/")
 public class UsuarioResource {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepo;
 	
-	@PostMapping 
+	//Inserindo novo Usuario
+	@PostMapping("/usuarios") 
 	public ResponseEntity<Usuario> addUsuario(@RequestBody Usuario usuario) {
 		try {
 			Usuario newUser = usuarioRepo.save(usuario);
@@ -28,5 +32,18 @@ public class UsuarioResource {
 		}
 	}
 	
+	//Buscando todos usuarios
+	@GetMapping("/usuarios")
+	public ResponseEntity <Iterable<Usuario>> listUsuarios() {
+		try {
+			Iterable<Usuario> usuarios = usuarioRepo.findAll();
+			if (((Collection<?>) usuarios).size() == 0) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(usuarios, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+	}
 
 }

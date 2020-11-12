fa36f6a92847e8f39ab45458e019a43a229e5d27
Higor <higornf2009@gmail.com>
2020-11-12 19:27:08 -0300

    Projeto iniciado!

---
+++ floripafretes/floripafretes/src/main/java/br/sc/senai/floripafretes/FloripafretesApplication.java
@@ -0,0 +1,13 @@
+package br.sc.senai.floripafretes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloripafretesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloripafretesApplication.class, args);
+	}
+
+}
---
+++ floripafretes/floripafretes/src/test/java/br/sc/senai/floripafretes/FloripafretesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sc.senai.floripafretes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloripafretesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

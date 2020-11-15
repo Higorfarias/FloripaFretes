8c91961c69e90b3fba50d805f0af9ff0af0cffa6
Higor <higornf2009@gmail.com>
2020-11-14 21:22:34 -0300

    Busca por id, PUT e DELETE

--- floripafretes/backend/src/main/java/br/sc/senai/floripafretes/resources/FreteResource.java
+++ floripafretes/backend/src/main/java/br/sc/senai/floripafretes/resources/FreteResource.java
@@ -1,12 +1,16 @@
 package br.sc.senai.floripafretes.resources;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +37,7 @@ public class FreteResource {
 	}
 	
 	@GetMapping("/fretes")
-	public ResponseEntity <Iterable<Frete>> listUsuarios() {
+	public ResponseEntity <Iterable<Frete>> listFretes() {
 		
 		try {
 			Iterable<Frete> fretes = freteRepo.findAll();
@@ -45,5 +49,40 @@ public class FreteResource {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 	}
+	
+	@GetMapping("/fretes/{id}")
+	public ResponseEntity<?> findById (@PathVariable("id") Integer id) {
+		return freteRepo.findById(id).map(record -> ResponseEntity.ok()
+				.body(record)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PutMapping("/fretes/{id}")
+	public ResponseEntity<Frete> updateFrete(@PathVariable("id") Integer id, @RequestBody Frete frete) {
+		Optional<Frete> freteData = freteRepo.findById(id);
+		
+		if(freteData.isPresent()) {
+			Frete updateFrete = freteData.get();
+			updateFrete.setTitulo(frete.getTitulo());
+			updateFrete.setDescricao(frete.getDescricao());
+			updateFrete.setEndereco(frete.getEndereco());
+			updateFrete.setCep(frete.getCep());
+			updateFrete.setCel(frete.getCel());
+			return new ResponseEntity<>(freteRepo.save(updateFrete), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping("fretes/{id}")
+	public ResponseEntity<HttpStatus> deleteFrete(@PathVariable("id") Integer id) {
+		
+		try {
+			freteRepo.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);	
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+	
 
 }

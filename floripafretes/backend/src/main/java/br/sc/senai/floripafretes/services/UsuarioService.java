198745d60478cc7d814b8f7c5f75902651031368
Higor <higornf2009@gmail.com>
2020-11-17 19:44:48 -0300

    Criando package exception e UsuarioService

---
+++ floripafretes/backend/src/main/java/br/sc/senai/floripafretes/services/UsuarioService.java
@@ -0,0 +1,51 @@
+package br.sc.senai.floripafretes.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.sc.senai.floripafretes.entities.Usuario;
+import br.sc.senai.floripafretes.exception.ResourceNotFoundException;
+import br.sc.senai.floripafretes.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepo;
+
+	public Usuario create(Usuario usuario) {
+		return usuarioRepo.save(usuario);
+	}
+
+	public List<Usuario> findAll() {
+		return usuarioRepo.findAll();
+	}
+
+	public Usuario findById(Integer id) {
+		return usuarioRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+	}
+
+	public Usuario update(Usuario usuario) {
+		Usuario entity = usuarioRepo.findById(usuario.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+		entity.setNome(usuario.getNome());
+		entity.setEmail(usuario.getEmail());
+		entity.setSenha(usuario.getSenha());
+		entity.setCelular(usuario.getCelular());
+		entity.setCep(usuario.getCep());
+		entity.setCep(usuario.getBairro());
+		
+		return usuarioRepo.save(entity);
+	}
+
+	public void delete(Integer id) {
+		Usuario entity = usuarioRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		usuarioRepo.delete(entity);
+	}
+
+}

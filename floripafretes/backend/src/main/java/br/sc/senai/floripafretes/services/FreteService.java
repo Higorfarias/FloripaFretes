65954a2bc87dc92a14e227858cddb1abd3ac500b
Higor <higornf2009@gmail.com>
2020-11-17 19:54:19 -0300

    Criacao do FreteService

---
+++ floripafretes/backend/src/main/java/br/sc/senai/floripafretes/services/FreteService.java
@@ -0,0 +1,49 @@
+package br.sc.senai.floripafretes.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.sc.senai.floripafretes.entities.Frete;
+import br.sc.senai.floripafretes.exception.ResourceNotFoundException;
+import br.sc.senai.floripafretes.repositories.FreteRepository;
+
+public class FreteService {
+	
+	@Autowired
+	private FreteRepository freteRepo;
+	
+	public Frete create(Frete frete) {
+		return freteRepo.save(frete);
+	}
+
+	public List<Frete> findAll() {
+		return freteRepo.findAll();
+	}
+
+	public Frete findById(Integer id) {
+		return freteRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+	}
+
+	public Frete update(Frete frete) {
+		Frete entity = freteRepo.findById(frete.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		entity.setTitulo(frete.getTitulo());
+		entity.setDescricao(frete.getDescricao());
+		entity.setEndereco(frete.getEndereco());
+		entity.setCep(frete.getCep());
+		entity.setCel(frete.getCel());
+		
+		return freteRepo.save(entity);
+	}
+
+	public void delete(Integer id) {
+		Frete entity = freteRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		freteRepo.delete(entity);
+	}
+
+
+}

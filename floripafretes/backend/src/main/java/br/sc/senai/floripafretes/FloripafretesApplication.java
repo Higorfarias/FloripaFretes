859af66fb59a8ef748e7e814668f7a0827c5d7c7
Higor <higornf2009@gmail.com>
2020-11-27 21:30:10 -0300

    Salvandi arquivo no S3 AWS

--- floripafretes/backend/src/main/java/br/sc/senai/floripafretes/FloripafretesApplication.java
+++ floripafretes/backend/src/main/java/br/sc/senai/floripafretes/FloripafretesApplication.java
@@ -1,13 +1,26 @@
 package br.sc.senai.floripafretes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.sc.senai.floripafretes.services.S3Service;
+
 @SpringBootApplication
-public class FloripafretesApplication {
+public class FloripafretesApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FloripafretesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		s3Service.uploadFile("C:\\Users\\higor\\Desktop\\Backup Notebook\\IMG_20200413_180223.jpg");
 	}
 
 	
